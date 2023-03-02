b2f2ed4c13ddcec9b37fbb9e55944bb0d39f4f7f
stephen ligtenberg <bountyhunter24@gmail.com>
2023-03-02 01:23:33 -0500

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        if self.freebies.find_by item_name: item_name
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev == self
+            freebie.update(dev: dev)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+comp1 = Company.create(name: "Google", founding_year: 1998)
+comp2 = Company.create(name: "Facebook", founding_year: 2004)
+comp3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+comp4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev1 = Dev.create(name: "Rick")
+dev2 = Dev.create(name: "Morty")
+dev3 = Dev.create(name: "Mr. Meseeks")
+dev4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name: "lanyard",        value: 1,  company_id: comp1.id, dev_id: dev2.id)
+Freebie.create(item_name: "keychain",       value: 3,  company_id: comp2.id, dev_id: dev1.id)
+Freebie.create(item_name: "sticker",        value: 5,  company_id: comp3.id, dev_id: dev2.id)
+Freebie.create(item_name: "bumper sticker", value: 2,  company_id: comp4.id, dev_id: dev4.id)
+Freebie.create(item_name: "candy",          value: 6,  company_id: comp1.id, dev_id: dev3.id)
+Freebie.create(item_name: "pen",            value: 10, company_id: comp2.id, dev_id: dev3.id)
+Freebie.create(item_name: "mug",            value: 9,  company_id: comp3.id, dev_id: dev2.id)
 
 puts "Seeding done!"
